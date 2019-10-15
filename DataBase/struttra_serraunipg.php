dd5666ff38ab25c77557dc8cd3dd42cbef2a31f1
Francesco <you@example.com>
2019-10-15 15:24:52 +0200

    Correzione file caricamento database, compilazione lista dei passi da fare per utulizzare l'applicazione nel file readmi.md

--- DataBase/struttra_serraunipg.php
+++ DataBase/struttra_serraunipg.php
@@ -21,21 +21,45 @@ $pdo = null;
 
 $db = "serraunipg";
 
-$utenti =  "CREATE TABLE utenti ("
-    . "username VARCHAR(255) PRIMARY KEY,"
-    . "nome VARCHAR(255),"
-    . "cognome VARCHAR(255),"
-    . "email VARCHAR(255),"
-    . "pwd VARCHAR(255),"
-    . "flag_admin INT NOT NULL DEFAULT '0',"
-    . "flag_npwd int(11) NOT NULL DEFAULT '0'"
-    . ")";
+$utenti =  "CREATE TABLE utenti (
+  username VARCHAR(255) NOT NULL PRIMARY KEY,
+  nome VARCHAR(255) DEFAULT NULL,
+  cognome VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(255) DEFAULT NULL,
+  pwd VARCHAR(255) DEFAULT NULL,
+  flag_admin INT(11) NOT NULL DEFAULT '0',
+  flag_newpwd INT(11) NOT NULL DEFAULT '0'
+)";
+
+$vaso = "CREATE TABLE vaso (
+  id_vaso INT PRIMARY KEY AUTO_INCREMENT,
+  pianta VARCHAR(255),
+  descrizione VARCHAR(255),
+  device_eui VARCHAR(255),
+  app_eui VARCHAR(255),
+  device_address VARCHAR(255),
+  ns_key VARCHAR(255),
+  as_key VARCHAR(255)
+)";
+
+$sensore = "CREATE TABLE sensore(
+  id_lettura INT PRIMARY KEY AUTO_INCREMENT,
+  data DATE,
+  ora TIME,
+  valore VARCHAR(255),
+  flag_centrale INT NOT NULL DEFAULT '0',
+  type_sensor INT NOT NULL,
+  id_vaso INT DEFAULT NULL,
+  FOREIGN KEY(id_vaso) REFERENCES vaso(id_vaso)
+)";
 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec($utenti);
+    $pdo->exec($vaso);
+    $pdo->exec($sensore);
 } catch (PDOException $e) {
    echo("Connection failed: " . $e->getCode());
 }
